c734821a94f9b05aacd6eef0451c4142be5f2b71
Debora Lara <wefit.debora@voeazul.com.br>
2026-02-27 17:45:00 -0300

    chore: adiciona tipagens para modulos remotos

---
+++ host/src/types/remotes.d.ts
@@ -0,0 +1,14 @@
+declare module 'remoteDashboard/Dashboard' {
+  const Dashboard: React.ComponentType
+  export default Dashboard
+}
+
+declare module 'remoteUsers/Users' {
+  const Users: React.ComponentType
+  export default Users
+}
+
+declare module 'remoteReports/Reports' {
+  const Reports: React.ComponentType
+  export default Reports
+}
